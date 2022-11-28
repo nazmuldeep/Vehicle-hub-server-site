d48b4705a9b4442b7212a9db61471e05ee4fb78f
nazmuldip <nazmuldip21@gmail.com>
2022-11-28 12:02:34 +0600

    package install and middle set

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import React from 'react';
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.Port || 5000;
+require('dotenv').config()
+// middleware using
+app.use(cors());
+app.use(express.json())
 
-const index = () => {
-    return (
-        <div>
-
-        </div>
-    );
-};
-
-export default index;
